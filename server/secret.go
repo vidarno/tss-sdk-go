7036c5a01d99066b6477ed1a55e2c3c5910fca30
danhale-git <danhale87@gmail.com>
2020-11-17 07:31:41 +0000

    SecretNameToID and MultipleSecretsFoundError added.

--- server/secret.go
+++ server/secret.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"strconv"
 )
 
@@ -23,6 +24,53 @@ type SecretField struct {
 	ItemID, FieldID, FileAttachmentID                      int
 	FieldDescription, FieldName, Filename, ItemValue, Slug string
 	IsFile, IsNotes, IsPassword                            bool
+}
+
+// SecretNameToID returns the ID of the named secret, if only a single exact match is found. If no exact match is found
+// an error is returned. If multiple exact matches are found an error of type MultipleSecretsFoundError is returned
+// containing the IDs for all matched secrets.
+func (s Server) SecretNameToID(name string) (int, error) {
+	filter := url.Values{
+		"filter.searchFieldSlug":     {"name"},
+		"filter.searchText":          {name},
+		"filter.doNotCalculateTotal": {"true"},
+	}.Encode()
+
+	if data, err := s.accessResource("GET", resource, "?"+filter, nil); err == nil {
+		// Declare structs in the local scope for unmarshaling
+		type record struct {
+			ID                 int
+			Name               string
+			SecretTemplateID   int
+			SecretTemplateName string
+		}
+
+		u := struct {
+			Records []record
+		}{}
+
+		// Unmarshal response
+		if err = json.Unmarshal(data, &u); err != nil {
+			return 0, fmt.Errorf("unmarshaling response: %s", err)
+		}
+
+		// Handle multiple returned values
+		switch {
+		case len(u.Records) > 1:
+			ids := make([]int, len(u.Records))
+			for i, r := range u.Records {
+				ids[i] = r.ID
+			}
+
+			return 0, MultipleSecretsFoundError{IDs: ids, searchedName: name}
+		case len(u.Records) < 1:
+			return 0, fmt.Errorf("no secrets found with name '%s'", name)
+		default:
+			return u.Records[0].ID, nil
+		}
+	} else {
+		return 0, fmt.Errorf("accessing resource %s: %s", resource, err)
+	}
 }
 
 // Secret gets the secret with id from the Secret Server of the given tenant
@@ -63,4 +111,14 @@ func (s Secret) Field(fieldName string) (string, bool) {
 	}
 
 	return "", false
+}
+
+// MultipleSecretsFoundError reports an error where multiple exact matches were found for a secret name search.
+type MultipleSecretsFoundError struct {
+	IDs          []int  // Secret IDs for all matched secrets
+	searchedName string // Name which matched the secrets
+}
+
+func (e MultipleSecretsFoundError) Error() string {
+	return fmt.Sprintf("multiple (%d) secrets found with name %s", len(e.IDs), e.searchedName)
 }
